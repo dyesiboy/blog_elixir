720918a387f0f5654ebfbea3497911c1531ec164
Jessie Arevalo <dt.jessie7@gmail.com>
2019-03-16 21:07:39 +0800

    Register User

--- lib/blog_web/resolvers/user_resolver.ex
+++ lib/blog_web/resolvers/user_resolver.ex
@@ -5,4 +5,8 @@ defmodule BlogWeb.Resolvers.UserResolver do
     {:ok, Accounts.list_users()}
 
   end
+
+  def register_user(_, %{input: input}, _) do
+    Accounts.create_user(input)
+  end
 end
--- lib/blog_web/schema.ex
+++ lib/blog_web/schema.ex
@@ -8,12 +8,16 @@ defmodule BlogWeb.Schema do
   query do
     @desc "Get all list of users"
     field :users, list_of(:user_type) do
-
       resolve(&Resolvers.UserResolver.users/3)
     end
   end
 
-  # mutation do
-  # end
+  mutation do
+    @desc "Register a new user"
+    field :register_user, type: :user_type do
+      arg(:input, non_null(:user_input_type))
+      resolve(&Resolvers.UserResolver.register_user/3)
+    end
+  end
 
 end
